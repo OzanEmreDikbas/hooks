24e87a0ca1f67db2a32a501eeff35244cd5d19af
Ozan Emre Dikbaş <87207946+OzanEmreDikbas@users.noreply.github.com>
2021-12-08 17:29:56 +0300

    4. added.

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,6 +1,41 @@
+import { Form, Button } from "react-bootstrap";
+
 const AddForm = () => {
 
     return (
-        <h1>Add Form</h1>  
+        <Form>
+            <Form.Group>
+                <Form.Control
+                type="text"
+                placeholder="Name *"
+                required 
+                />
+            </Form.Group>
+            <Form.Group>
+                <Form.Control
+                type="email"
+                placeholder="Email *"
+                required 
+                />
+            </Form.Group>
+            <Form.Group>
+                <Form.Control
+                as="textarea"
+                placeholder="Address *"
+                rows={3} 
+                />
+            </Form.Group>
+            <Form.Group>
+                <Form.Control
+                type="number"
+                placeholder="Phone *" 
+                />
+            </Form.Group>
+            <Button variant="success" type="submit" block>
+                Add New Employee
+            </Button>
+        </Form>  
     )
 }
+
+export default AddForm;
--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,27 +1,69 @@
-import { useContext } from "react";
+/** @format */
+
+import { useContext, useState } from "react";
 import Employee from "./Employee";
 import { EmployeeContext } from "../contexts/EmployeeContext";
+import {Button, Modal, ModalBody, ModalFooter, ModalTitle} from "react-bootstrap";
+import ModalHeader from "react-bootstrap/esm/ModalHeader";
+import AddForm from "./AddForm";
 
 const EmployeeList = () => {
 
-  const {employees} = useContext(EmployeeContext)
+  const { employees } = useContext(EmployeeContext);
+  const [show, setShow] = useState(false)
 
-  
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
-    <table className='table table-striped table-hover'>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Address</th>
-          <th>Phone</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        <Employee employees={employees}/>
-      </tbody>
-    </table>
+    <>
+      <div className='table-title'>
+        <div className='row'>
+          <div className='col-sm-6'>
+            <h2>
+              Manage <b>Employees</b>
+            </h2>
+          </div>
+          <div className='col-sm-6'>
+            <Button
+              onClick={handleShow}
+              className='btn btn-success text white'
+              data-toggle='modal'>
+              <i className='material-icons'>&#xE147;</i>{" "}
+              <span>Add New Employee</span>
+            </Button>
+          </div>
+        </div>
+      </div>
+      <table className='table table-striped table-hover'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Address</th>
+            <th>Phone</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <Employee employees={employees} />
+        </tbody>
+      </table>
+      <Modal show={show} onHide={handleClose}>
+        <ModalHeader className="model-header" closeButton>
+          <ModalTitle>
+            Add Employee
+          </ModalTitle>
+        </ModalHeader>
+        <ModalBody>
+          <AddForm />
+        </ModalBody>
+        <ModalFooter>
+          <Button variant="secondary" onClick={handleClose}>
+            Close Modal
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </>
   );
 };
 export default EmployeeList;
